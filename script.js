fdfb70c71bc873bbca5448185df1447bbd007c51
Halina <gella93@gmail.com>
2017-10-19 10:14:58 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var movies = [
+/*var movies = [
 	{
 		id: 1,
 		title: 'Harry Potter',
@@ -39,5 +39,55 @@ var element =
 		React.createElement('h1', {}, 'Lista filmów'),
 		React.createElement('ul', {}, moviesElements)
 	);
+	*/
+
+
+/*var GalleryItem = React.createClass({
+	propTypes: {
+		image: React.PropTypes.object.isRequired,
+	},
+	render: function() {
+		return (
+		React.createElement('div', {},
+			React.createElement('h2', {}, this.props.image.name),
+			React.createElement('img', {src: this.props.image.src})
+			)
+		)	
+	},
+});
+
+var image = {
+	name: '',
+	src: ''
+};
+
+var element = React.createElement(GalleryItem, {image: image});	*/
+
+
+
+var Movie = React.createClass({
+	propTypes: {
+		movie: React.PropTypes.object.isRequired,
+	},
+	render: function() {
+		return (
+			React.createElement('div', {},
+			React.createElement('h2', {}, this.props.movie.id),
+			React.createElement('h2', {}, this.props.movie.title),
+			React.createElement('p', {}, this.props.movie.desc),
+			React.createElement('img', {src: this.props.movie.src})
+			)
+		);	 	
+	}
+});
+
+var movie = {
+	id: 1,
+	title: 'Game of Thrones',
+	desc: 'Fantasy',
+	src: 'http://www.hbo.pl/siteimages/articleimages/13000/13043_-_254.jpg',
+};
+
+var element = React.createElement(Movie, {movie: movie});
 
 ReactDOM.render(element,document.getElementById('app'));
