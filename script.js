3f23d97e362d2b9ce8a88a71082eae2e53674842
Halina <gella93@gmail.com>
2017-10-20 12:55:09 +0200

    update

--- script.js
+++ script.js
@@ -5,22 +5,77 @@ var Movie = React.createClass({
 	render: function() {
 		return (
 			React.createElement('div', {},
-			React.createElement('h2', {}, this.props.movie.id),
-			React.createElement('h2', {}, this.props.movie.title),
-			React.createElement('p', {}, this.props.movie.desc),
-			React.createElement('img', {src: this.props.movie.src})
+				React.createElement(MovieTitle, {title:this.props.movie.title}),
+				React.createElement(MovieDescription, {description:this.props.movie.desc}),
+				React.createElement(MoviePhoto, {photo: this.props.movie.src})
 			)
 		);	 	
 	}
 });
 
-var movie = {
-	id: 1,
-	title: 'Game of Thrones',
-	desc: 'Fantasy',
-	src: 'http://www.hbo.pl/siteimages/articleimages/13000/13043_-_254.jpg',
-};
+var MovieTitle = React.createClass({
+	propTypes: {
+		title: React.PropTypes.string.isRequired,
+	},
+	render: function() {
+		return React.createElement('h2', {}, this.props.title)
+	}
+});
 
-var element = React.createElement(Movie, {movie: movie});
+var MovieDescription = React.createClass({
+	propTypes: {
+		description: React.PropTypes.string.isRequired,
+	},
+	render: function() {
+		return React.createElement('p', {}, this.props.description)
+	}
+});
+
+var MoviePhoto = React.createClass({
+	propTypes: {
+		photo: React.PropTypes.string.isRequired,
+	},
+	render: function() {
+		/*console.log('Moje foto', this.props);*/
+		return React.createElement('img', {src: this.props.photo})
+	}
+});
+
+var MovieList = React.createClass({
+	propTypes: {
+		list: React.PropTypes.array.isRequired,
+	},
+	render: function() {
+		var movieList = this.props.list.map(function(movie) {
+			return React.createElement(Movie, {key: movie.id, movie:movie})
+		});
+		return (
+			React.createElement('div',{}, movieList)
+			)
+	}
+})
+
+var movies = [
+	{
+		id: 1,
+		title: 'Game of Thrones',
+		desc: 'Fantasy',
+		src: 'http://www.hbo.pl/siteimages/articleimages/13000/13043_-_254.jpg',
+	},
+	{
+		id: 2,
+		title: 'Arrow',
+		desc: 'Sci-Fi',
+		src: 'http://data.whicdn.com/images/101904233/large.jpg',
+	},
+	{
+		id: 3,
+		title: 'Super Girl',
+		desc: 'Sci-Fi',
+		src: 'https://i.pinimg.com/736x/85/04/79/850479481bc2d9d32caa8b9de2e6bee8--melissa-supergirl-supergirl-series.jpg',
+	}
+]
+
+var element = React.createElement(MovieList, {list:movies});
 
 ReactDOM.render(element,document.getElementById('app'));
